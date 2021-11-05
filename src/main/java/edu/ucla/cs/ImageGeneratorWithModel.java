943286c3767d36a3d033f31d8c6cddcd44bf9863
usama54321 <usama54321@gmail.com>
2021-11-04 17:32:48 -0700

    clean up image generator with model

--- src/main/java/edu/ucla/cs/ImageGeneratorWithModel.java
+++ src/main/java/edu/ucla/cs/ImageGeneratorWithModel.java
@@ -24,7 +24,6 @@ import java.awt.Image;
 
 public class ImageGeneratorWithModel extends Generator<Img> {
     private static MtcnnService mtcnnService = null;
-    private static int COUNT = 0;
     private static String[] IMAGES = null;
 
     public ImageGeneratorWithModel() {
@@ -46,31 +45,26 @@ public class ImageGeneratorWithModel extends Generator<Img> {
     @Override
     public Img generate(SourceOfRandomness random, GenerationStatus t) {
         String workDir = System.getProperty("user.dir");
-        mtcnnService = new MtcnnService(30, 0.709, new double[] { 0.6, 0.7, 0.7 });
-
-        //System.out.printf("Count %d\n", COUNT);
-        if (IMAGES == null)
-            IMAGES = this.initialize(workDir + "/images");
-
-        int w = App.getWidth(), h = App.getHeight();
-        //java.util.Random rnd = new java.util.Random(random.nextInt());
-        int[] data = Util.getRandomImage(random, w, h);
-
-        BufferedImage inputImage = null;
         try {
-            inputImage = ImageIO.read(new File(IMAGES[COUNT]));
-        } catch (Exception e) {
+        if (mtcnnService == null) {
+            mtcnnService = new MtcnnService(30, 0.709, new double[] { 0.6, 0.7, 0.7 });
+        }
+        }catch(Exception e) {
             e.printStackTrace();
             return null;
         }
-        /*
+
+        int w = App.getWidth(), h = App.getHeight();
+        int[] data = Util.getRandomImage(random, w, h);
+
+        BufferedImage inputImage = null;
+
         inputImage = new BufferedImage(w, h, BufferedImage.TYPE_3BYTE_BGR);
         for (int i =0 ; i < h; i++) {
             for(int j = 0; j < w; j++) {
                 inputImage.setRGB(i, j, data[h * i  + j]);
             }
         }
-        */
 
         FaceAnnotation[] faceAnnotations = null;
 
@@ -83,14 +77,19 @@ public class ImageGeneratorWithModel extends Generator<Img> {
 
         //@FIXIT only single annotations for now?
         BoundingBox b = faceAnnotations[0].getBoundingBox();
-        BufferedImage croppedImage = inputImage.getSubimage(b.getX(), b.getY(), b.getW(), b.getH());
+        BufferedImage croppedImage;
+        try {
+            croppedImage = inputImage.getSubimage(b.getX(), b.getY(), b.getW(), b.getH());
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new Img(data);
+        }
 
         BufferedImage resized = Util.resizeImage(croppedImage, w, h);
         int[] pixels = resized.getRGB(0,0,256,256, null, 0, 256);//new int[h * w];
 
         Img im = new Img(pixels);
 
-        ImageGeneratorWithModel.COUNT += 1;
         return im;
     }
 }
